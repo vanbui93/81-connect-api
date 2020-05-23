8388b0dd246f356f098f6c4b2c1e7fbf0b04301d
unknown <you@example.com>
2020-05-23 17:06:15 +0700

    [react-router] tich hop vao project

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,38 @@
 import React, { Component } from 'react'
+import { Route, Link } from "react-router-dom";
+
+const menus = [
+  {
+    name: 'Trang chủ',
+    to: '/',
+    exact: true
+
+  },
+  {
+    name: 'Quản lý sản phẩm',
+    to: '/product/list',
+    exact: false
+  }
+];
+
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+  return (
+    <Route
+      path={to}
+      exact={activeOnlyWhenExact}
+      children={({ match }) => {
+        var active = match ? 'active' : '';
+        return (
+          <li className={`nav-item ${active}`}>
+            <Link to={to} className="nav-link">
+              {label}
+            </Link>
+          </li>
+        )
+      }}
+    />
+  )
+}
 
 export default class Menu extends Component {
   render() {
@@ -6,14 +40,26 @@ export default class Menu extends Component {
       <nav className="navbar navbar-expand navbar-light bg-light">
         <a className="navbar-brand">Call API</a>
         <ul className="nav navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">Trang chủ <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/product/list">Quản lý sản phẩm</a>
-          </li>
+          {this.showMenus(menus)}
         </ul>
       </nav>
     )
+  }
+
+  showMenus = (menus) => {
+    var result = null;
+    if (menus.length > 0) {
+      result = menus.map((menu, index) => {
+        return (
+          <MenuLink
+            key={index}
+            label={menu.name}
+            to={menu.to}
+            activeOnlyWhenExact={menu.exact}
+          />
+        )
+      })
+    }
+    return result;
   }
 }
